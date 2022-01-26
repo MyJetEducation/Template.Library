c96128f32093a9b497af0bb6dcc5269b8361664a
dmitriy.b <dmitriy.b@smplt.net>
2022-01-26 14:09:29 +0500

    Solution added

---
+++ template/README.md
@@ -0,0 +1,10 @@
+# Service.SampleLibrary
+
+![Release Service](https://github.com/MyJetWallet/Service.SampleLibrary/workflows/Release%20Service/badge.svg)
+
+![Release API client nuget](https://github.com/MyJetWallet/Service.SampleLibrary/workflows/Release%20API%20client%20nuget/badge.svg)
+
+![CI test build](https://github.com/MyJetWallet/Service.SampleLibrary/workflows/CI%20test%20build/badge.svg)
+
+*Client library:* ![Nuget version](https://img.shields.io/nuget/v/MyJetWallet.Service.SampleLibrary.Client?label=MyJetWallet.Service.SampleLibrary.Client&style=social)
+
---
+++ template/test/TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using ProtoBuf.Grpc.Client;
+using Service.AssetsDictionary.Client;
+using Service.AssetsDictionary.Grpc.Models;
+
+namespace TestApp
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GrpcClientFactory.AllowUnencryptedHttp2 = true;
+
+            Console.Write("Press enter to start");
+            Console.ReadLine();
+
+            var factory = new AssetsDictionaryClientFactory("http://localhost:5001");
+            var client = factory.GetAssetsDictionaryService();
+
+            var resp = await  client.SayHelloAsync(new HelloGrpcRequest(){Name = "Alex"});
+            Console.WriteLine(resp?.Message);
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+    }
+}
